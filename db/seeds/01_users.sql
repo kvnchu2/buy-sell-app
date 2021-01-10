ac9ed9101f2da2e25ef2b1c6111498ad8fb951ac
deonleer <deon.leer96@gmail.com>
2021-01-10 23:59:42 +0000

    more seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name) VALUES ('Deon');
+INSERT INTO users (name) VALUES ('Kevin');
+INSERT INTO users (name) VALUES ('Colin');
