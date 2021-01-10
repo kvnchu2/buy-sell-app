b32ce82b712799edcc0649ce3fca4e6232682124
deonleer <deon.leer96@gmail.com>
2021-01-10 23:28:10 +0000

    Added tables

---
+++ db/schema/05_favourites.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS favourites CASCADE;
+CREATE TABLE favourites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  item_id INTEGER REFERENCES items(id) NOT NULL
+)
