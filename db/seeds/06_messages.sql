fa30e0c5252613604ee95863d36068c9e3ed369e
deonleer <deon.leer96@gmail.com>
2021-01-13 18:18:34 +0000

    merged colin's branch

--- db/seeds/06_messages.sql
+++ db/seeds/06_messages.sql
@@ -1,4 +1,4 @@
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Hello i wanna buy this thing');
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'ok give me 100 thousand dollars please');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Hello I wanna buy this');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'OK give me 100 thousand dollars please.');
 INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'ight here u go');
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'thx');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'thx!');
